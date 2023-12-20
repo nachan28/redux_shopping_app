77198cbdcbf8a7170a1b6eeef05e83ae34f0efd3
nachan28 <torajiro282828@gmail.com>
2023-12-21 08:47:21 +0900

    add reducers to cartSlice

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -2,15 +2,47 @@ import { createSlice } from "@reduxjs/toolkit";
 import cartItems from "../../cartItems";
 
 const initialState = {
-  cartItem: cartItems,
-  amount: 0,
+  cartItems: cartItems,
+  amount: 4,
   total: 0,
 };
 
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {},
+  reducers: {
+    clearCart: (state) => {
+      return { cartItem: [], amount: 0, total: 0 };
+    },
+    removeItem: (state, action) => {
+      console.log(action);
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
+    increase: (state, action) => {
+      const cartItem = state.cartItems.find(
+        (item) => item.id === action.payload
+      );
+      cartItem.amount = cartItem.amount + 1;
+    },
+    decrease: (state, action) => {
+      const cartItem = state.cartItems.find(
+        (item) => item.id === action.payload
+      );
+      cartItem.amount = cartItem.amount - 1;
+    },
+    calculateTotals: (state, action) => {
+      let amount = 0;
+      let total = 0;
+      state.cartItems.forEach(item => {
+        amount += item.amount;
+        total += item.price * item.amount;
+      })
+      state.amount = amount;
+      state.total = total;
+    }
+  },
 });
 
+export const { clearCart, removeItem, increase, decrease, calculateTotals } = cartSlice.actions;
 export default cartSlice.reducer;
