85efa058f33b981c0ce29ffe78ff88d00b788eeb
nachan28 <torajiro282828@gmail.com>
2023-12-20 15:34:50 +0900

    just format

--- src/cartItems.js
+++ src/cartItems.js
@@ -1,32 +1,32 @@
 const cartItems = [
-    {
-      id: 1,
-      title: "Next.js入門講座",
-      price: "12000",
-      img: "https://uploda1.ysklog.net/uploda/3f3dc982dc.png",
-      amount: 1,
-    },
-    {
-      id: 2,
-      title: "MERNスタックで本格的なSNSアプリ構築講座",
-      price: "24000",
-      img: "https://uploda1.ysklog.net/uploda/bf73156667.png",
-      amount: 1,
-    },
-    {
-      id: 3,
-      title: "GraphQLとApollo入門講座",
-      price: "8900",
-      img: "https://uploda1.ysklog.net/uploda/9a99f8ca04.png",
-      amount: 1,
-    },
-    {
-      id: 4,
-      title: "Three.jsでモダンウェブサイト構築講座",
-      price: "14000",
-      img: "https://uploda1.ysklog.net/uploda/59060b3cd3.png",
-      amount: 1,
-    },
-  ];
-  
-  export default cartItems;
+  {
+    id: 1,
+    title: "Tomato",
+    price: "12000",
+    img: "https://i.ibb.co/Qry0WJQ/fd400555.jpg",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "Carrot",
+    price: "24000",
+    img: "https://i.ibb.co/Bcw3Lrz/fd400317.jpg",
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "Squash",
+    price: "8900",
+    img: "https://i.ibb.co/frVfkG8/fd400237.jpg",
+    amount: 1,
+  },
+  {
+    id: 4,
+    title: "Corn",
+    price: "14000",
+    img: "https://i.ibb.co/tXCfjbW/fd400184.jpg",
+    amount: 1,
+  },
+];
+
+export default cartItems;
